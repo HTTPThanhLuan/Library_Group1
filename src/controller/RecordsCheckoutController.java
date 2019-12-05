e59a55a5b838772236d377e8c917314e891de542
ThanhLuan <luan.tt87@gmail.com>
2019-12-05 17:30:07 -0600

    fix bug

--- src/controller/RecordsCheckoutController.java
+++ src/controller/RecordsCheckoutController.java
@@ -27,7 +27,7 @@ import java.nio.IntBuffer;
 import java.util.ResourceBundle;
 
 
-public class RecordsCheckoutController extends Application implements Initializable {
+public class RecordsCheckoutController {
 
     // The table and columns
     @FXML
@@ -85,8 +85,9 @@ public class RecordsCheckoutController extends Application implements Initializa
     }
 
 
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
+    @FXML
+	private void initialize() {
+   
 
 
         // Set up the table data Book Checkout==================================================
@@ -250,212 +251,5 @@ public class RecordsCheckoutController extends Application implements Initializa
             return false;
         }
     }
-
-    @Override
-    protected Object _enterNestedEventLoop() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    protected int _getKeyCodeForChar(char arg0) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    protected void _invokeAndWait(Runnable arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    protected void _invokeLater(Runnable arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    protected void _leaveNestedEventLoop(Object arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    protected boolean _supportsTransparentWindows() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-
-    @Override
-    protected boolean _supportsUnifiedWindows() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-
-    @Override
-    public Cursor createCursor(int arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public Cursor createCursor(int arg0, int arg1, Pixels arg2) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public Pixels createPixels(int arg0, int arg1, ByteBuffer arg2) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public Pixels createPixels(int arg0, int arg1, IntBuffer arg2) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-	@Override
-	public Pixels createPixels(int i, int i1, IntBuffer intBuffer, float v, float v1) {
-		return null;
-	}
-
-
-	@Override
-    public Timer createTimer(Runnable arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public View createView() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public Window createWindow(long arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public Window createWindow(Window arg0, Screen arg1, int arg2) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    protected void runLoop(Runnable arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    protected FileChooserResult staticCommonDialogs_showFileChooser(Window arg0, String arg1, String arg2, String arg3,
-                                                                    int arg4, boolean arg5, ExtensionFilter[] arg6, int arg7) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    protected File staticCommonDialogs_showFolderChooser(Window arg0, String arg1, String arg2) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    protected Size staticCursor_getBestSize(int arg0, int arg1) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    protected void staticCursor_setVisible(boolean arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    protected int staticPixels_getNativeFormat() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-	@Override
-	public GlassRobot createRobot() {
-		return null;
-	}
-
-
-	@Override
-    protected Screen[] staticScreen_getScreens() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    protected double staticScreen_getVideoRefreshPeriod() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    protected int staticTimer_getMaxPeriod() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    protected int staticTimer_getMinPeriod() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    protected int staticView_getMultiClickMaxX() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    protected int staticView_getMultiClickMaxY() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    protected long staticView_getMultiClickTime() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
 
 }
